a33adf4252363647c04ad6e37d9931c2e4f05333
NaudCode <naudcode@gmail.com>
2025-08-26 19:18:52 +0200

    Change : - Siret, Mail contact. - Services 2 first box - delete section "ils nous ont fait confiance" - add carousel / switch with classic photo.

--- js/script.js
+++ js/script.js
@@ -82,5 +82,11 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
 
+    const swiper = new Swiper(".swiper", {
+        loop: true,
+        autoplay: { delay: 4000 },
+        pagination: { el: ".swiper-pagination", clickable: true },
+        navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+    });
 });
 
